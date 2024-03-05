23be3b1d44e61baf801f187a78636a49b50added
blessyzp <112764994+blessyzp@users.noreply.github.com>
2024-03-05 20:50:52 +0800

    Assignment1
    part1:编写一个实现计算器的控制台程序，接受两个数字和一个运算符作为输入，并打印出计算结果。 
    part2:编写一个实现计算器的 Windows 窗体应用程序：在两个文本框中输入数字，选择运算符，点击 "计算 "按钮，然后显示结果。

---
+++ Assignment1/Assignment1/Program_calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program_calculator
+    {
+        static void Main(string[] args)
+        {
+            //准备输入
+            double a = 0.0, b = 0.0, c = 0.0;
+            string s = "", str = "";
+
+            //准备取数据
+            Console.Write("Please input two double numbers: ");
+            s = Console.ReadLine();
+            a = Double.Parse(s);
+            s = Console.ReadLine();
+            b = Double.Parse(s);
+            Console.Write("Please input the operator: ");
+            while(str == "")
+            {
+                str = Console.ReadLine();
+                if ((str != "+" && str != "-" && str != "/" && str != "*" && str != "**") || (str == "/" && b == 0) )
+                {
+                    if(!(str == "/" && b == 0))
+                        Console.Write("The input is invalid. Please input the operator again: ");
+                    else
+                    {
+                        Console.Write("The input is invalid. Please input the number b and operator again: ");
+                        s = Console.ReadLine();
+                        b = Double.Parse(s);
+                    }
+                    str = "";
+                }
+
+            }
+
+            //开始处理
+            switch (str)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+                case "**":
+                    c = Math.Pow(a,b);
+                    break;
+                default: break;
+            }
+
+            //输出值
+            Console.WriteLine($"{a} {str} {b} = {c}");
+
+            return;
+        }
+    }
+}
---
+++ Assignment1/Program_calculator_WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_calculator_WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //准备数据输入
+        string str = "", s = "";
+        double a = 0.0, b = 0.0, c=0.0;
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            str = this.comboBox1.Text;
+
+
+            if (str == null)
+            {
+                MessageBox.Show("SelectedValue is null.");
+                str = "";
+            }
+
+            switch (str)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+                case "**":
+                    c = Math.Pow(a, b);
+                    break;
+                default: break;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                s = textBox1.Text;
+                if (s == "")
+                    a = 0.0;
+                else a = System.Convert.ToDouble(s);
+                break;
+
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                s = textBox2.Text;
+                if (s == "")
+                    b = 0.0;
+                else b = System.Convert.ToDouble(s);
+                break;
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            comboBox1_SelectedIndexChanged(sender, e);
+            while (str == "/" && b == 0)
+            {
+                MessageBox.Show("The input is invalid. Please input the number b and operator again: ");
+                s = textBox2.Text;
+                b = Double.Parse(s);
+                comboBox1_SelectedIndexChanged(sender, e);
+            }
+
+            textBox3.Text = c.ToString();
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
