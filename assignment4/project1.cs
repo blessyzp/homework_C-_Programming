de9867c27082817f9ea33256976804bdb9dc9b67
blessyzp <112764994+blessyzp@users.noreply.github.com>
2024-03-27 11:53:22 +0800

    Add files via upload

---
+++ assignment4/project1.cs
@@ -0,0 +1,99 @@
+﻿namespace assignment4;
+
+
+
+public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+}
+
+
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        tail = head = null;
+    }
+
+    public Node<T> Head
+    {
+        get => head;
+    }
+
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+
+
+
+
+}
+
+
+public class Program1
+{
+    static void Main(string[] args)
+    {
+        GenericList<int> myList = new GenericList<int>();
+
+        //先简单创建几个数据加进去
+        myList.Add(100);
+        myList.Add(150);
+        myList.Add(200);
+
+        myList.ForEach(item => Console.WriteLine(item));
+
+        int min = myList.Head.Data;
+        myList.ForEach(item =>
+        {
+            if (item < min)
+                min = item;
+        });
+        Console.WriteLine($"Min: {min}");
+
+        int max = myList.Head.Data;
+        myList.ForEach(item =>
+        {
+            if (item > max)
+                max = item;
+        });
+        Console.WriteLine($"Max: {max}");
+
+        int sum = 0;
+        myList.ForEach(item => sum += item);
+        Console.WriteLine($"Sum: {sum}");
+
+
+    }
+}
+
---
+++ assignment4/project2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace assignment4
+{
+
+    public class ClockEventArgs : EventArgs
+    {
+        public DateTime CurrentTime { get; }
+
+        public ClockEventArgs(DateTime currentTime)
+        {
+            CurrentTime = currentTime;
+        }
+    }
+
+    
+
+    public class Clock
+    {
+
+        //定义委托
+        public delegate void ClockEventHandler(object sender, ClockEventArgs e);
+        
+        //Tick事件
+        public event ClockEventHandler Tick;
+
+        //Alarm事件
+        public event ClockEventHandler Alarm;
+
+        private System.Timers.Timer timer;
+        private DateTime alarmTime;
+
+        public Clock(int interval, DateTime alarmTime)
+        {
+            this.alarmTime = alarmTime;
+
+            timer = new System.Timers.Timer(interval);
+
+            timer.Elapsed += OnTick;
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+        private void OnTick(object sender, ElapsedEventArgs e)
+        {
+            Tick?.Invoke(this, new ClockEventArgs(DateTime.Now));
+
+            if (DateTime.Now >= alarmTime)
+            {
+                Alarm?.Invoke(this, new ClockEventArgs(DateTime.Now));
+                Stop();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime alarmTime = DateTime.Now.AddSeconds(10);
+
+            Clock clock = new Clock(1000, alarmTime);
+
+            clock.Tick += (sender, e) => Console.WriteLine($"Tick: {e.CurrentTime.ToLongTimeString()}");
+            clock.Alarm += (sender, e) => Console.WriteLine($"Alarm: {e.CurrentTime.ToLongTimeString()} - Time to wake up!");
+
+            clock.Start();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
